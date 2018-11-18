f11846c1c11c468b1a8299b6e39d0a87d6caeea7
Roman <neveroff606@gmail.com>
2018-11-18 21:31:04 +0600

    initial commit

---
+++ dictinary.py
@@ -0,0 +1,59 @@
+import csv
+import random
+from datetime import datetime
+
+from prob import load_data
+
+def fill_dictionary(words):
+    more = '1'
+    while more == "1":
+        print('вводим новые слова в словарь')
+        date_now = datetime.now()
+        eng_word = input('введите слово на ')
+        if eng_word in words:
+            print('слово 1есть в словаре ')
+            continue
+        rus_word = input('введите перевод ')
+        words[eng_word] = rus_word
+        more = input('ввести еще слово, 1 да, 0 нет.')
+        dict_file = open('dict.csv', 'a')
+        w = csv.writer(dict_file)
+        w.writerow([eng_word, rus_word, date_now])
+        dict_file.close()
+
+words = load_data()
+
+mode = input('вводим словарик - y, no - нафиг ')
+if mode == 'y':
+    fill_dictionary(words)
+
+
+
+
+datetime.today()
+
+
+print('сейчас проверим слова')
+more = '1'
+while more == '1':
+    key, trans_value = random.choice(list(words.items()))
+    print('введите перевод слова {word}'.format(word=key))
+    input_value = input('введите перевод ')
+    if input_value == trans_value:
+        print('зер гуд')
+    else:
+        print('учи дальше')
+
+    more = input('еще слово или спать пойдешь? 1-да, 0-нет')
+
+
+
+# дата ввода слов.
+# проверка на повторы
+# режим проверки слов.
+# радомная проверка слов
+# проверить 5 слов перед вводом слов
+
+
+
+
---
+++ prob.py
@@ -0,0 +1,31 @@
+import csv
+
+
+more = '1'
+def load_data():
+    data = {}
+    with open('dict.csv') as f:
+        read_file = csv.reader(f)
+        for row in read_file:
+            if not row:#проверка на пропущенные строки
+                continue
+            key = row[0]
+            walue = row[1]
+            data[key] = walue#присваивание переменных с ключем и значением
+    return data
+
+if __name__ =="__main__":
+    words = load_data()
+
+    dict_file = open('dict.csv', 'a')
+    w = csv.writer(dict_file)
+    print("проверка слов")
+    for i in range(4):
+
+        key = input('введите английское слово ')
+        value = input('введите перевод ')
+        if key in words and value == words[key]:#сверяем значение с ключем
+            print("правильно")
+        else:
+            print('ошибка')
+
